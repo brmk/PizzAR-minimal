fe7193f73e70966630e61295b3825cd935260517
Ihor Barmak <ihor.barmak@gmail.com>
2018-04-04 18:24:46 +0300

    added build script

--- .bin/build.sh
+++ .bin/build.sh
@@ -5,11 +5,11 @@ rm -rf ./.build
 meteor build ./.build --server https://pizzar-brmk.herokuapp.com --mobile-settings ./settings.json
 
 # sign android app using private key
-# jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 ./.build/android/release-unsigned.apk pestle -keystore ./.bin/pestle.keystore
-# $ANDROID_HOME/build-tools/25.0.3/zipalign 4 ./.build/android/release-unsigned.apk ./.build/android/release.apk
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 ./.build/android/release-unsigned.apk pizzar -keystore ./.bin/pizzar.keystore
+$ANDROID_HOME/build-tools/25.0.3/zipalign 4 ./.build/android/release-unsigned.apk ./.build/android/release.apk
 
-# jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 ./.build/android/project/build/outputs/apk/release/android-release-unsigned.apk pestle -keystore ./.bin/pestle.keystore
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 ./.build/android/project/build/outputs/apk/release/android-release-unsigned.apk pizzar -keystore ./.bin/pizzar.keystore
 
-# $ANDROID_HOME/build-tools/25.0.3/zipalign 4 ./.build/android/project/build/outputs/apk/release/android-release-unsigned.apk ./.build/android/release.apk
+$ANDROID_HOME/build-tools/25.0.3/zipalign 4 ./.build/android/project/build/outputs/apk/release/android-release-unsigned.apk ./.build/android/release.apk
 
 open ./.build/
